25b846079599fde4fe44bbbedcab524e7389b3e5
snimu <sebastian.nicolas.mueller@gmail.com>
2024-10-11 10:07:16 +0200

    run exp with mult heads, shorter seed

--- experiments.sh
+++ experiments.sh
@@ -2,4 +2,4 @@
 python main.py -c --logfile results1.csv -w --wandb_project att-norm.1 --qk_activ gelu none --qk_norm fro_norm rms_norm none --attn_activ softmax sigmoid tanh none --post_attn_norm rms_norm none --num_runs 5 --seed 12345
 
 # go through the baseline and the best three other settings at 240M parameters
-python main.py -c --logfile results2.csv -w --wandb_project att-norm.2 --qk_activ none --qk_norm fro_norm rms_norm none --attn_activ softmax sigmoid --post_attn_norm rms_norm none --num_runs 5 --seed 12345 --depth 21 --width 1024
+python main.py -c --logfile results2.csv -w --wandb_project att-norm.2 --qk_activ none --qk_norm fro_norm rms_norm none --attn_activ softmax sigmoid --post_attn_norm rms_norm none --num_runs 5 --seed 1 --depth 21 --width 1024 --num_heads 16
